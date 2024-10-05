b7b609dae523ab0ae219ab2f44e5bbdd082d9c63
jorbort <jorbort92@gmail.com>
2024-10-05 15:09:45 +0200

    server listo presentacion de la app y empzando a lidiar con canvas

---
+++ nginx/js/index.js
@@ -0,0 +1,48 @@
+loadIntro();
+
+
+function loadIntro(){
+	let maindiv = document.getElementById('main');
+let scroll_container = document.createElement('div');
+scroll_container.className = 'scroll-container'; 
+let text = document.createElement('div');
+text.className = 'scroll-text';
+text.id = 'scroll-text';
+text.innerHTML = `The world is changing—and so are the skies, the seas, and the very air we breathe. In this interactive journey, you’ll face one of humanity’s greatest challenges: global warming, driven by the rising levels of greenhouse gases (GHG) in the atmosphere. The decisions you make will determine the fate of the planet. Will you guide humanity towards a sustainable future, or lead us deeper into environmental chaos?
+
+From renewable energy policies to lifestyle choices, every action has consequences. The balance between economic development and environmental protection will be tested as you navigate critical moments in history, make decisions for communities, and influence global leaders. Your choices could mitigate the effects of climate change—or hasten its impact.
+
+You are not just an observer, but an active participant in shaping the Earth’s future. Are you ready to take on the challenge? The world awaits your leadership. Choose wisely, because the planet’s survival is in your hands.`;
+scroll_container.appendChild(text);
+maindiv.appendChild(scroll_container);
+
+text.addEventListener('animationend', () => {
+	scroll_container.className = 'centered';
+	scroll_container.innerHTML = `<img src="https://media.tenor.com/n0A3c_iyR8IAAAAM/explode-cute.gif" alt="Explosion GIF" id="michi">`;
+	
+	setTimeout(() => {
+		scroll_container.innerHTML = `<canvas id="gameCanvas"></canvas>`;
+		initializeGameCanvas();
+	  }, 3000); 
+});
+}
+
+function loadImageToCanvas(imageUrl, canvas){
+	let img = new Image();
+	img.src = imageUrl;
+	img.onload = function() {
+		canvas.context.drawImage(img, 0,0, canvas.width, canvas.height);
+	};
+}
+
+function initializeGameCanvas() {
+	let canvas = document.getElementById('gameCanvas');
+	canvas.width = 1400;
+	canvas.height = 1000;
+	let context = canvas.getContext('2d');
+	context.fillStyle = 'black';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3vPf0xPuRDg5StjluYqcQUZFYnuQAnChPh5DQdQFuQvny4W4ZBpzacTKGSXrkXzn3thI&usqp=CAU'
+	
+}
